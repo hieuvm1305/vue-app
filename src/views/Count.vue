777c10aea4e420e73b203db156cf7e1c94285898
hieuvm1305 <Hieuvm1305@gmail.com>
2025-01-03 08:58:57 +0700

    first commit

---
+++ src/views/Count.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Button label="Secondary" severity="secondary" raised />
+    <div class="flex flex-row items-center">
+      <div>This is count value:</div>
+      <div clas="p-2">{{ count }}</div>
+    </div>
+    <div class="p-2">
+      <div class="flex items-center">
+        <div class="mr-2">Increment</div>
+        <Button @click="increment" label="Tang" severity="secondary"></Button>
+      </div>
+      <div class="flex items-center">
+        <div class="mr-2">Increment</div>
+        <Button @click="decrement" severity="secondary">Giam</Button>
+      </div>
+      <div class="flex items-center">
+        <InputNumber v-model="count_number"/>
+        <Button @click="setCountValue()" severity="secondary">set value</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
+import Button from "primevue/button";
+import InputNumber from "primevue/inputnumber";
+
+const countStore = useCounterStore();
+const { count } = storeToRefs(countStore);
+const { increment, decrement, setValue } = countStore;
+const count_number = ref(0)
+function setCountValue() {
+    setValue(count_number.value)
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Button from "primevue/button";
+import InputNumber from "primevue/inputnumber";
+import { ref } from "vue";
+
+const count_number = ref(0);
+
+function setCountValue() {
+  console.log("Count value set to:", count_number.value);
+}
+</script>
+
+<template>
+  <div class="flex items-center">
+    
+    <InputNumber v-model="count_number" />
+    <Button @click="setCountValue()" severity="secondary" label="Set Value" />
+  </div>
+</template>
